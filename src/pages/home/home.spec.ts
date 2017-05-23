d4d3982a23bf1c306af89785341a7364f96bd8ea
Aldrin Tingson <aldrintingson@gmail.com>
2017-05-23 16:12:34 +0800

    added test. remove mock library as it is not working

--- src/pages/home/home.spec.ts
+++ src/pages/home/home.spec.ts
@@ -7,14 +7,22 @@ import {
 import { HomePage } from './home';
 import { NavControllerMock, NavParamsMock, AlertControllerMock } from 'ionic-mocks';
 import {ProjectProvider} from "../../providers/project/project";
-import {instance, mock} from "ts-mockito";
 import {PlatformMock} from "../../../test-config/mocks-ionic";
+import {Observable} from "rxjs";
+import {Project} from "../../model/project";
+import {Validator} from "validator.ts/Validator";
 
 describe('HomePage', () => {
   let fixture;
-  let component;
+  let component: HomePage;
+  let projectProvider = <ProjectProvider>{};
+  let navController = NavControllerMock.instance();
+  let alertController = AlertControllerMock.instance();
+  let navParams = NavParamsMock.instance();
 
   beforeEach(async(() => {
+    projectProvider = <ProjectProvider>{};
+
     TestBed.configureTestingModule({
       declarations: [ HomePage ],
       imports: [
@@ -22,10 +30,10 @@ describe('HomePage', () => {
       ],
       providers: [
         //mocking since they are actual dependency of the Homepage component
-        { provide: NavController,  useValue: NavControllerMock.instance() },
-        { provide: NavParams,  useValue: NavParamsMock.instance() },
-        { provide: ProjectProvider,  useValue: instance(mock(ProjectProvider)) },
-        { provide: AlertController,  useValue: AlertControllerMock.instance() },
+        { provide: NavController,  useValue: navController },
+        { provide: NavParams,  useValue: navParams },
+        { provide: ProjectProvider,  useValue: projectProvider },
+        { provide: AlertController,  useValue: alertController },
 
         //test fixture that needs to be mock
         { provide: Platform,  useClass: PlatformMock },
@@ -49,4 +57,29 @@ describe('HomePage', () => {
   it ('should create a valid instance of HomePage', () => {
     expect(component instanceof HomePage).toBe(true);
   });
+
+  it ('should show error message when project is not valid', () => {
+    component.saveOrUpdateProject();
+    expect(alertController.create).toHaveBeenCalled();
+  });
+
+  it ('should have an instance of project when passed to params is null', () => {
+    expect(component.project).toBeTruthy();
+  });
+
+  it ('should have shown an alert message when provider throws error', () => {
+    projectProvider.saveOrUpdate = (p: Project) => Observable.throw("some errors");
+    expect(alertController.create).toHaveBeenCalled();
+  });
+
+  // it ('should go back when saving a valid project', () => {
+  //   let validator = new Validator();
+  //   let errors = validator.validate(this.project);
+  //   component.project.name = "test";
+  //   expect(errors.length).toEqual(0);
+  //
+  //   projectProvider.saveOrUpdate = (p: Project) => Observable.of(component.project);
+  //   expect(alertController.create).toHaveBeenCalledTimes(0);
+  //   expect(navController.pop).toHaveBeenCalled();
+  // });
 });
